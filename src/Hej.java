0bb42d66ea565a56eeef44aac3a3c504e67ba4a3
Javier <me@xavidiaz.com>
2020-09-08 09:15:15 +0200

    Let's begin!

--- src/Hej.java
+++
@@ -1,8 +0,0 @@
-/* This is my first Java Class
-Hej Javi!
- */
-public class Hej {
-    public static void main(String[] args) {
-        System.out.println("Hej Javi!"); // print out String
-    }
-}

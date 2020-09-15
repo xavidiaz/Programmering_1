659e5587e05e693655b03cdefbf97696bb784b39
Javier <me@xavidiaz.com>
2020-09-15 12:19:14 +0200

    Övning 2 Slut 02 DONE!

---
+++ src/book/OvningTvaSlut_02.java
@@ -0,0 +1,49 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org>
+ *
+ */
+
+package book;
+
+import javax.swing.*;
+
+/**
+ * Skriv ett program som från en dialogruta läser in: Efternamn, Förenamn.
+ * Visa imput med följande format "Hej Förenamn!"
+ * Namn ska visas i stora bokstäver.
+ */
+public class OvningTvaSlut_02 {
+    public static void main(String[] arg){
+        // Input Dialogruta
+        String anvandare = JOptionPane.showInputDialog(null, "Skriv Efternamn, Förenamn");
+        // get length
+        int i = anvandare.indexOf(' ');
+        int s = anvandare.length();
+        // Initiera namn med första bokstav veral
+        String namn =  anvandare.substring(i + 1, i + 2).toUpperCase() + anvandare.substring(i + 2, s).toLowerCase();
+        JOptionPane.showMessageDialog(null, "Hej, " + namn);
+    }
+}

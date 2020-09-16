8fbfece6a5cb1b55cf4bad0345fb8467bfdca15c
Javier <me@xavidiaz.com>
2020-09-16 09:27:11 +0200

    Övning 3 01 DONE!

---
+++ src/book/Otre_01.java
@@ -0,0 +1,50 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org>
+ *
+ */
+
+package book;
+
+/**
+ * Skriv ett program där du deklarerar variablerna i, d, och b på
+ * samma sätt som i detta avsnitt.
+ * Skriv sedan tre tilldelningssatser där du försöker tilldela
+ * värdet 500 till var och en av variablerna.
+ * Försök kompilera programmet! Vilka tilldelningar var tillåtna?
+ * Om någon var otillåten försök varför.
+ */
+public class Otre_01 {
+    public static void main(String[] arg){
+        // deklarerar i, d, och b
+        int i;
+        double d;
+        byte b;
+        i = 500; // integers tillåter heltal 32 antal bytar
+        d = 500; // doubles tillåter reellatal 64 antal bytar
+        //b = 500; // Felt! byte tillåter 8 antar bytar.
+
+    }
+}

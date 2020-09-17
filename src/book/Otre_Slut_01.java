caf1f8bca6fb60d9a52c5daec22468e1c9e7d2b0
Javier <me@xavidiaz.com>
2020-09-17 09:58:18 +0200

    Övning 3 Slut 1 Done!

---
+++ src/book/Otre_Slut_01.java
@@ -0,0 +1,59 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org>
+ *
+ */
+
+package book;
+
+import javax.swing.*;
+
+/**
+ * Skriv ett program som beräknar volymen och arean av en sfär.
+ * Som indata, i en dialogruta, ges sfären radie. *
+ */
+public class Otre_Slut_01 {
+    public static void main(String[] arg) {
+        // variables
+        String iRadie;
+        double radie, vol, area, pi;
+        pi = Math.PI;
+        // Inputruta
+        iRadie = JOptionPane.showInputDialog(null,"Skriv radie:");
+        radie = Double.parseDouble(iRadie);
+        // metoder
+        area =(int) ((4 * pi * Math.pow(radie, 3))/3);
+        vol = (int)(4 * pi * Math.pow(radie, 2));
+        // dialogruta
+        JOptionPane.showMessageDialog(null,
+                "Volymen: " + vol + "." +
+                        "\nArea: " + area + "",
+                "Sfär",
+                JOptionPane.INFORMATION_MESSAGE);
+
+    }}
+
+
+

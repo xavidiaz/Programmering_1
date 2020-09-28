4a7926d7c28cb1fc8e899ae77443c615f26e09ba
Javier <me@xavidiaz.com>
2020-09-28 10:35:54 +0200

    Have to check: 5_4 & 5_slut_6

--- src/book/OfyraSult_06.java
+++ src/book/OfyraSult_06.java
@@ -38,6 +38,35 @@ import javax.swing.*;
  * triangel och vinkeln mellan sidorna. Programmet skall avgöra om
  * triangel är liksidig, likbent eller oliksidig.
  *
+ * @<code>//
+ * Filen Triangel.java
+ *
+ * import javax.swing.*;
+ *
+ * public class Triangel {
+ *   public static void main(String[] arg) {
+ *     String s = JOptionPane.showInputDialog("Längden för första sidan?");
+ *     double a = Double.parseDouble(s);
+ *
+ *     s = JOptionPane.showInputDialog("Längden för andra sidan?");
+ *     double b = Double.parseDouble(s);
+ *
+ *     s = JOptionPane.showInputDialog("Vinkeln mellan sidorna?");
+ *     double v = Double.parseDouble(s);
+ *     double gamma = v * 2 * 3.1415926536 / 360;  // omräkning till radianer
+ *
+ *     double c = Math.sqrt(a*a + b*b - 2*a*b*Math.cos(gamma));
+ *     double e = 1e-10;  // ett litet tal
+ *
+ *     if (Math.abs(a-b) < e && Math.abs(a-c) < e && Math.abs(b-c) < e)
+ *       JOptionPane.showMessageDialog(null, "Liksidig");
+ *     else if (Math.abs(a-b) < e || Math.abs(a-c) < e || Math.abs(b-c) < e)
+ *       JOptionPane.showMessageDialog(null, "Likbent");
+ *     else
+ *       JOptionPane.showMessageDialog(null, "Oliksidig");
+ *   }
+ * }</code>
+ *
  */
 public class OfyraSult_06 {
     public static void main(String[] arg){
@@ -66,11 +95,12 @@ public class OfyraSult_06 {
                         "vinkeln:",
                         title, JOptionPane.INFORMATION_MESSAGE)
         );
-        // till int
+        // v till radianer
+        double vRad = v * 2 * 3.1415926536 / 360;
 
         // formel
         c = Math.sqrt(
-                (a * a) + (b * b) - (2 * a * b * Math.cos(v))
+                (a * a) + (b * b) - (2 * a * b * Math.cos(vRad))
         );
 
         // Dialogruta

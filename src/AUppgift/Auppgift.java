7b02b333f68a352076d68b39c4ba4678d7f136df
Javier <me@xavidiaz.com>
2020-10-09 22:28:17 +0200

    B Uppgift Done

--- src/AUppgift/Auppgift.java
+++ src/AUppgift/Auppgift.java
@@ -30,6 +30,20 @@ package AUppgift;
 
 import javax.swing.*;
 
+/**
+ * Instruktioner
+ *
+ * Skapa ett program som beräknar en persons statliga inkomstskatt.
+ * Läs in årsinkomsten i programmet och beräkna skatten.
+ * För att underlätta beräkningen, använd det lägsta grundavdraget (13 400 kr).
+ * OBS! Om årsinkomsten är mindre än den nedre skiktgränsen
+ * så behöver man inte betala statlig inkomstskatt.
+ *
+ * Användaren ska kunna beräkna statlig inkomstskatt för flera personer (årsinkomster)
+ * utan att behöva starta om programmet.
+ * Det innebär att programmet ska fråga användaren om den vill utföra ytterligare
+ * en skatteberäkning när en skatteberäkning har utförts och redovisats för användaren.
+ */
 public class Auppgift {
     public static void main(String[] args) {
         // Variablern
@@ -47,58 +61,73 @@ public class Auppgift {
                 lGrundavdrag = 13400;// lägsta grundavdraget (13 400 kr).
         String
                 meddelande;
-
-        // Årsinkomst
-        meddelande = arsinkomstInput;
-        while (true) {
-            try {
-                arsinkomst = Float.parseFloat(
-                        JOptionPane.showInputDialog(null,
-                                meddelande, title, JOptionPane.INFORMATION_MESSAGE)
-                );
-                break; // om inga undantag bryts loop
-            } // validering med anpasan meddelande.
-            catch (NumberFormatException e) {
-                if(e.getMessage().equals("empty String")){
-                    meddelande =  InputNull + arsinkomstInput;
-                } else {
-                    System.out.print(e.getMessage());
-                    meddelande = "Du skrev: " + e.getMessage().substring(e.getMessage().lastIndexOf(" ")+1) + "\n" + NoNumber + arsinkomstInput;
-                }
-            }
-        }
-        if(lGrundavdrag > arsinkomst){
-            // Dialogruta under det lägsta grundavdraget (13 400 kr).
-            JOptionPane.showMessageDialog(null,
-                    "Du ligger under den lägsta grundavdraget (13 400 kr).\n" +
-                            "Du ska inte betala någon skatt.");
-        } else {
-            // Skatt
-            meddelande = skattInput;
+        int fler = 0;
+        while(fler == 0){
+            // Årsinkomst
+            meddelande = arsinkomstInput;
             while (true) {
                 try {
-                    skatt = Float.parseFloat(
+                    arsinkomst = Float.parseFloat(
                             JOptionPane.showInputDialog(null,
                                     meddelande, title, JOptionPane.INFORMATION_MESSAGE)
                     );
                     break; // om inga undantag bryts loop
-                    // validering med anpasan meddelande.// validering med anpasan meddelande.
-                } catch (NumberFormatException e) {
+                } // validering med anpasan meddelande.
+                catch (NumberFormatException e) {
                     if(e.getMessage().equals("empty String")){
-                        meddelande =  InputNull + skattInput;
+                        meddelande =  InputNull + arsinkomstInput;
                     } else {
                         System.out.print(e.getMessage());
-                        meddelande = "Du skrev: " + e.getMessage().substring(e.getMessage().lastIndexOf(" ")+1) + "\n" + skatteProcent + skattInput;
+                        meddelande = "Du skrev: " + e.getMessage().substring(e.getMessage().lastIndexOf(" ")+1) + "\n" + NoNumber + arsinkomstInput;
                     }
                 }
             }
-            // Beräkning
-            float attBetala = arsinkomst * skatt / 100;
+            if(lGrundavdrag > arsinkomst){
+                // Dialogruta under det lägsta grundavdraget (13 400 kr).
+                JOptionPane.showMessageDialog(null,
+                        "Du ligger under den lägsta grundavdraget (13 400 kr).\n" +
+                                "Du ska inte betala någon skatt.");
+            } else {
+                // Skatt
+                meddelande = skattInput;
+                while (true) {
+                    try {
+                        skatt = Float.parseFloat(
+                                JOptionPane.showInputDialog(null,
+                                        meddelande, title, JOptionPane.INFORMATION_MESSAGE)
+                        );
+                        break; // om inga undantag bryts loop
+                        // validering med anpasan meddelande.// validering med anpasan meddelande.
+                    } catch (NumberFormatException e) {
+                        if(e.getMessage().equals("empty String")){
+                            meddelande =  InputNull + skattInput;
+                        } else {
+                            System.out.print(e.getMessage());
+                            meddelande = "Du skrev: " + e.getMessage().substring(e.getMessage().lastIndexOf(" ")+1) + "\n" + skatteProcent + skattInput;
+                        }
+                    }
+                }
+                // Beräkning
+                float attBetala = arsinkomst * skatt / 100;
 
-            // Dialog ruta summa att betala
-            JOptionPane.showMessageDialog(null,
-                    "Du får betala: " + (int)attBetala + " kr.",
-                    title, JOptionPane.INFORMATION_MESSAGE);
+                // Dialog ruta summa att betala
+                JOptionPane.showMessageDialog(null,
+                        "Du får betala: " + (int)attBetala + " kr.",
+                        title, JOptionPane.INFORMATION_MESSAGE);
+            }
+
+            // Fråga om beräkna igen
+            // Messadedialog
+            int igen = JOptionPane.showConfirmDialog (null, "Önskar du beräkna igen?",
+                    title, JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+            if(igen == JOptionPane.YES_OPTION) {
+                fler = 0;
+                System.out.print(igen);
+            } else {
+                fler = 1;
+                System.out.print(igen);
+            }
         }
+
     }
 }
